52b57735774ec834047b3ebac3b9be42c77dd296
unknown <saeedqirat43@gmail.com>
2025-05-02 19:22:15 +0500

    refactor

--- app.py
+++ app.py
@@ -249,8 +249,8 @@ st.markdown("""
             <br>
             Senior Student
         <div class='social-icons'>
-            <a href="https://github.com/qiratsumra"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></a>
-            <a href="https://www.linkedin.com/in/qirat-saeed-8048662b7/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png"></a>
+            <a href="https://github.com/qiratsumra"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png target="_blank""></a>
+            <a href="https://www.linkedin.com/in/qirat-saeed-8048662b7/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" target="_blank"></a>
         </div>
             </div>
 """, unsafe_allow_html=True)
